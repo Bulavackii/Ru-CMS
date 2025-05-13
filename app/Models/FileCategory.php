7de913221125d3078efb04ecd321dbb3d031dbf5
Den <visitorsec@gmail.com>
2025-05-13 08:44:52 +0300

    переписал миграции

--- app/Models/FileCategory.php
+++ app/Models/FileCategory.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class FileCategory extends Model
 {
-    // Указываем поля, которые могут быть массово присвоены
-    protected $fillable = ['name', 'icon'];
+    protected $table = 'file_categories';  // Является ли это именем вашей таблицы?
+
+    // Если есть какие-то связи, например, с моделью File:
+    public function files()
+    {
+        return $this->hasMany(File::class, 'category_id'); // Убедитесь, что правильно указаны поля
+    }
 }
